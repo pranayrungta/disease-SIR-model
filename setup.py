fb9bb3ddf2d9617db30b341ed6972ce375194f0d
Pranay <pranay.rungta@gmail.com>
2020-04-03 12:04:55 +0530

    setup.py dependency added

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup
 
 setup( name='sir_model',
        packages=['sir_model'],
+       author="Pranay, Kanishka",
+       author_email="pranay.rungta@gmail.com",
+       install_requires=['numpy', 'pyqt5', 'matplotlib']
        entry_points={
            'console_scripts': ['sir_model=sir_model.main:main'] } )
